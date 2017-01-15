66a30b69f7cbad15a99e59160c21938e75ecf4f2
jede <jede@oric.org>
2017-01-15 22:15:53 +0100

    removing Story book filename extension

--- Storybook2/Buildsb.bat
+++ Storybook2/Buildsb.bat
@@ -8,7 +8,7 @@ echo Building telemon Version (no header)
 %osdk%\bin\xa.exe %INPUTFN%.s -o release\telemon.out -e xaerr.txt -l %INPUTFN%.txt -DTARGET_TELEMON
 
 echo Building telemon Version for Orix
-%osdk%\bin\xa.exe %INPUTFN%.s -o release\tolsb.com -e xaerr.txt -l %INPUTFN%.txt -DTARGET_TELEMON -DTARGET_ORIX 
+%osdk%\bin\xa.exe %INPUTFN%.s -o release\tolsb -e xaerr.txt -l %INPUTFN%.txt -DTARGET_TELEMON -DTARGET_ORIX 
 
 echo Building atmos Version (no header)
 %osdk%\bin\xa.exe %INPUTFN%.s -o release\final.out -e xaerr.txt -l %INPUTFN%.txt
